8ac2e9f0f707e78100f19544a30055d780846541
ZianTT <53261506+ZianTT@users.noreply.github.com>
2024-07-07 21:23:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def run(hyg):
                 elif status == 8:
                     logger.warning(i18n_format("pro_tem_sold_out"))
                 start_time = time.time()
-                while time.time() - start_time < 150:
+                while time.time() - start_time < 150 or hyg.sold_out:
                     if hyg.try_create_order():
                         if "hunter" not in hyg.config:
                             hyg.sdk.capture_message("Pay success!")
