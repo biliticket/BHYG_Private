d2910dda881398d68698b5c86a2da8baa8079760
ZianTT <53261506+ZianTT@users.noreply.github.com>
2024-07-07 08:01:57 +0000

    bump version to 0.8.11

--- globals.py
+++ globals.py
@@ -21,7 +21,7 @@ from utils import prompt, save, load
 import time
 from i18n import *
 
-ver_int = 810
+ver_int = 811
 version = "v{}.{}.{}".format(
     (ver_int // 10000) % 100, (ver_int // 100) % 100, ver_int % 100
 )
