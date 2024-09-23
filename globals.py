415941c6b54a5218926f865306b5008980b7beb3
ZianTT <53261506+ZianTT@users.noreply.github.com>
2024-09-23 12:53:46 +0000

    use only system time

--- globals.py
+++ globals.py
@@ -325,10 +325,6 @@ def load_config():
     if "cover_time_offset" in config:
         logger.info(i18n_format("cover_time_offset"))
         logger.info(i18n_format("time_offset").format(config["time_offset"]))
-    else:
-        logger.info(i18n_format("auto_time_offset"))
-        config["time_offset"] = -0.5
-        logger.info(i18n_format("time_offset").format(-0.5))
     while True:
         if "cookie" not in config or not use_login:
             config["cookie"] = interactive_login(sentry_sdk)
